320ccea32ea5f7fa0932581cde36877226fbbc82
Gabriel Tiburcio <tibuurcio@gmail.com>
2017-04-27 07:26:40 -0300

    Recuperando tweets do servidor

--- src/App.js
+++ src/App.js
@@ -1,48 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
-
 import Tweet from './componentes/Tweet';
 
-var tweets = [{
-  usuario: {
-		nome: "Gabriel",
-		sobrenome: "Tiburcio",
-    foto: "https://avatars3.githubusercontent.com/u/5959178?v=3&s=460"
-	},
-	data_publicacao: "20/04/2017",
-	texto: "Javascript parece daora, acho que todos deveriam aprender.",
-	likes: 3
-}, 
-{
-  usuario: {
-		nome: "Diego",
-		sobrenome: "Cabral",
-    foto: "http://placehold.it/100x100"
-	},
-	data_publicacao: "20/04/2017",
-	texto: "Realmente, Javascript tem cara de futuro.",
-	likes: 3
-}, 
-{
-  usuario: {
-		nome: "Orivaldo",
-		sobrenome: "Santana",
-    foto: "http://placehold.it/100x100"
-	},
-	data_publicacao: "20/04/2017",
-	texto: "Prefiro C++",
-	likes: 0
-}];
-
 class App extends React.Component {
-  render() {
-    var listaTweets = tweets.map(function(tweet) {
-      return <Tweet tweet={tweet}/>
-    });
+	constructor(props) {
+		super(props);
+		this.state = {
+			listaTweets: null
+		};
+	}
 
+	componentDidMount() {
+		axios.get('http://localhost:3004/tweets')
+			.then((resposta) => this.carregarTweets(resposta));
+	}
+
+	carregarTweets(resposta) {
+		console.log(resposta.data);
+		const listaTweets = resposta.data.map(function(tweet) {
+			return <Tweet tweet={tweet}/>
+		});
+		this.setState({ listaTweets: listaTweets });
+	}
+
+  render() {
+		const tweets = this.state.listaTweets;
     return (
       <div className="App">
-        { listaTweets }
+        { !tweets ? 'Nenhum tweet registrado' : tweets }
       </div>
     );
   }
